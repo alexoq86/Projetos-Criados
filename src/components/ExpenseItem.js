78be24f383e47eb7d318fd6b21439660e5eba989
Alexandre Oliveira Queiroz <alezoq86@gmail.com>
2021-09-27 17:26:15 -0400

    Components 1.0

---
+++ src/components/ExpenseItem.js
@@ -0,0 +1,12 @@
+import React from 'react';
+function ExpenseItem() {
+    return (<div>
+        <div>Date</div>
+        <div>
+            <h2>Car Insurance</h2>
+            <div>$295</div>
+        </div>
+    </div>)
+}
+
+export default ExpenseItem;
