f4abc13738f4f0beb8a44501f3813015ba2d4b21
Alexandre Oliveira Queiroz <alezoq86@gmail.com>
2021-10-06 11:22:20 -0400

    BarChart funcionando

--- src/components/Expenses/Expenses.js
+++ src/components/Expenses/Expenses.js
@@ -3,15 +3,16 @@ import React, { useState } from "react";
 import Card from '../UI/Card';
 import ExpensesFilter from './ExpensesFilter';
 import ExpensesList from './ExpensesList';
+import ExpensesChart from './ExpensesChart';
 
 function Expenses(props) {
 
-  const [enteredFilter, setEnteredFilter] = useState("2020");
+  const [enteredFilter, setEnteredFilter] = useState("2021");
 
   const newFilter = filterSelected => {
     setEnteredFilter(filterSelected)
   };
-
+  
   const filteredExpenses = props.items.filter(expense => {
     return expense.date.getFullYear().toString() === enteredFilter;
   });
@@ -23,6 +24,7 @@ function Expenses(props) {
           selected={enteredFilter}
           onSelectFilter={newFilter}
         />
+        <ExpensesChart expenses={filteredExpenses}/>
         <ExpensesList items={filteredExpenses}/>
       </Card>
     </div>
